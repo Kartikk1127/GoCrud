4e7854a382aa85e47606e756812c110dda14447e
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-07-07 16:12:04 +0530

    add request validations

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/Kartikk1127/GoCrud/internal/config"
+	"github.com/Kartikk1127/GoCrud/internal/http/handlers/student"
 )
 
 func main() {
@@ -21,9 +22,7 @@ func main() {
 	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 	// setup server
 
 	server := http.Server{
